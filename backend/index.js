b61815d432d92543e4433bfe42ae920fe77a3e9b
Milla22 <marti.ludmilla@orange.fr>
2024-08-12 23:12:54 +0200

    Ajout du dossier backend avec suppression du .git

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+//const CloudinaryServer = require('ldla-server-uploader-cloudinary');
+
+app.listen(3001, () => {
+    console.log('Server running on port 3001');
+});
