ff5e52394fb345a5fea0b99caa82dd2fb862a47d
Milla22 <marti.ludmilla@orange.fr>
2024-08-14 22:14:02 +0200

    modif cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
+const express = require('express');
 require('dotenv').config();
-
-
+const cors = require('cors');
 const { start } = require('ldla-server-uploader-cloudinary');
 
+const app = express();
+app.use(cors({
+    origin: 'http://localhost:3001', // Remplacez par l'origine de votre front-end si nécessaire
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 
 // Démarre le serveur fourni par le composant //
